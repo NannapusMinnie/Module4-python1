1721a3cb972ebd14c9efea5e15c9cae783abf61e
NannapusMinnie <nannapus.tiya@gmail.com>
2025-10-21 14:40:18 +0700

    lecture

--- day2.py
+++ day2.py
@@ -1,1 +1,32 @@
 print('Hello, World!')
+print('Goodbye') 
+# print output the input in the terminal area
+#1 print fuction = 1 output line in the terminal
+
+# comments are text that will not be runned by the program
+'''codes are runned line by line , order matters
+# is used for 1 line comment
+3 single quotes are used for multiline comments
+'''
+
+'''every fucntion call begins with a fucntion name and parenthesis. Inputs are separated by comma
+fucntion_name(input_1,input_2, input_3)
+argument is input of function
+'''
+print(1,2,6,'hello') #each element is separated by a space in the terminal
+print(1,2,6,'hello',sep='-') # separated by -
+
+
+# data type
+# use type() to chech data type of a function
+a = type(56)
+print('a is',a)
+
+print(type(-2.0))
+''' 
+str (string) = text single and double quotes are used to tell the program that what inside is a string eg. 'hello', '5.00'
+int (integer) = whole number (no decimal points) eg. 3, 5, -4, 101
+float (floating point number) = number with decimal points eg. 3.0, 3.5, 0.64
+bool (boolean) = the truth value True, False
+None (NoneType) = no data (it means lack of data)
+'''
