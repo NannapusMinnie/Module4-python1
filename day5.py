66a1741c0d355545d9390211581de822f7bd8be6
NannapusMinnie <nannapus.tiya@gmail.com>
2025-10-24 14:33:53 +0700

    in -task class

---
+++ day5.py
@@ -0,0 +1,26 @@
+''' x = float(input())
+if x % 2 == 0 and not x % 3 == 0:
+    print('B')
+elif x % 2 ==0 and x % 3 == 0:
+    print('C')
+else:
+    print('D') '''
+
+x = float(input())
+
+if x % 2 == 0:
+    if x % 3 != 0:
+        print('B')
+    else:
+        print('C')
+else:
+    print('D')
+
+
+'''
+input x
+if x is an even number, print('A')
+if x is even and isn't divisible by 3, print('B')
+if x is even and divisible by 3, print('C')
+if x is odd print('D')
+'''
